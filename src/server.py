ac1b641e6daebcd0467be85c5d91522776509c44
alouilouai <louaialoui1993@gmail.com>
2024-12-07 17:45:48 +0100

    adding socket service to wrap the socket logic

--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@ from flask_socketio import SocketIO
 from src.database import DatabaseService, db
 from flask_cors import CORS
 from src.models.user import User
+from src.services.socket_service import SocketService
 
 app = Flask(__name__)
 
@@ -24,26 +25,11 @@ socketio = SocketIO(app, cors_allowed_origins=["http://localhost:3000"])
 # Enable CORS for your Next.js app on localhost:3000
 CORS(app, origins=["http://localhost:3000"])
 
-@socketio.on('connect')
-def handle_connect():
-    print('A user connected')
-    socketio.emit('server_message', {'message': 'Welcome to the chat server!'})
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('A user disconnected')
-    
-@socketio.on('send_message')
-def handle_send_message(data):
-    """
-    Listen for a 'send_message' event from a client and broadcast it to everyone.
-    """
-    print(f"Message received: {data}")
-    socketio.emit('receive_message', data, to=None)
+socket_service = SocketService(app)
 
 @app.route("/")
 def index():
     return jsonify({'message': 'Chat server is running!'})
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0')
+    socket_service.run()
