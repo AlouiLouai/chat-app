3372ec7d21463effca8b0cbe068ce187528c75c4
alouilouai <louaialoui1993@gmail.com>
2024-12-14 23:41:02 +0100

    made dockerfiles for render

--- src/server.py
+++ src/server.py
@@ -61,4 +61,5 @@ def index():
     return jsonify({'message': 'Chat server is running!'})
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
